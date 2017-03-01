db1713833c8da74b3c85bccc0d75b8a64027d077
Leon Jun Wei Lam <llam@olin.edu>
2017-03-01 01:49:28 -0500

    testing out basic api-database functionality

---
+++ Api.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import psycopg2
+import os
+import urllib.parse
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+
+#CHECK THE PROCFILE! NOT ENTIRELY SURE IF WE CAN RUN THIS AS WORKER
+
+def make_conn():
+    global url
+    conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port)
+    return conn
+
+def interact_with_database(instruction, debug=False):
+    """
+    debug = True: returns a string that tells you what you just did.
+    debug = False: returns only cursor contents.
+    """
+    store = None
+    conn = make_conn()
+    with conn.cursor() as cur:
+        # try:
+        cur.execute(instruction)
+        store = [row for row in cur]
+        # except:
+            # pass
+    conn.commit()
+    conn.close()
+    if debug:
+        if store:
+            return "Your instruction was " + str(instruction) + " . Cursor output (if any) is: " + str(store)
+        else:
+            return "Your instruction was " + str(instruction) + " . No cursor output."
+    else:
+        return store
+
+
+class TodoSimple(Resource):
+    """a get request can have a request.form e.g. 
+    get('http://localhost:5000/todo1', data = {'test': 'testing'})"""
+    def get(self, todo_id):
+        if request.form: 
+            print request.form
+            return interact_with_database(request.form['query'])
+        else:
+            return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
