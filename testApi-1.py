bf1ba3758289750b4cad4380aa012a49efd7a6f6
Leon Jun Wei Lam <llam@olin.edu>
2017-03-01 01:58:41 -0500

    syntax changes to api.py, restoring commented lines in testapi in case

--- testApi-1.py
+++ testApi-1.py
@@ -1,10 +1,3 @@
 from requests import put, get
-# print put('http://localhost:5000/todo1', data={'data': 'Remember the milk'}).json()
-
-# get('http://localhost:5000/todo1').json()
-
-# print put('http://localhost:5000/todo2', data={'data': 'Change my brakepads'}).json()
-
-# get('http://localhost:5000/todo2').json()
 
 get('http://olin-api.herokuapp.com/todo1', data = {'query': 'select * from events where event_id is not null'})
