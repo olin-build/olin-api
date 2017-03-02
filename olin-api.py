d9aa2de183cc0b2f2e66ffd778318934c6a37a4a
Leon Jun Wei Lam <llam@olin.edu>
2017-03-01 21:34:45 -0500

    working get/put/post requests with form-encoded data support

--- olin-api.py
+++ olin-api.py
@@ -1,58 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-
-import psycopg2
 import os
-import urllib.parse
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {'todo1':'take out trash'}
 
-urllib.parse.uses_netloc.append("postgres")
-url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
-
-
 parser = reqparse.RequestParser()
+
 parser.add_argument('data')
 parser.add_argument('a')
-
-
-#CHECK THE PROCFILE! I THINK WE HAVE TO RUN THIS AS WEB
-
-def make_conn():
-    global url
-    conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port)
-    return conn
-
-def interact_with_database(instruction, debug=False):
-    """
-    debug = True: returns a string that tells you what you just did.
-    debug = False: returns only cursor contents.
-    """
-    store = None
-    conn = make_conn()
-    with conn.cursor() as cur:
-        # try:
-        cur.execute(instruction)
-        store = [row for row in cur]
-        # except:
-            # pass
-    conn.commit()
-    conn.close()
-    if debug:
-        if store:
-            return "Your instruction was " + str(instruction) + " . Cursor output (if any) is: " + str(store)
-        else:
-            return "Your instruction was " + str(instruction) + " . No cursor output."
-    else:
-        return store
+parser.add_argument('c')
 
 
 """
@@ -61,51 +20,40 @@ The Resource then handles it and sends a Response back (it seems like it has to 
 """
 # http://flask.pocoo.org/docs/0.12/api/#flask.Request
 
+
+def doSomething(query_dict):
+    return sorted(query_dict)
+
+def searchSomething(query):
+    if True:
+        try:
+            return "Found something relating to " + str(query) + "!"
+        except:
+            pass
+    return "Nothing was found."
+
+
 class TodoSimple(Resource):
     def get(self, todo_id):
-        if request:
-            try:
-                return str(request.__dict__)
-            except:    
-                return request
-        else:
-            return "get request."
+        return searchSomething(todo_id)
 
     def put(self, todo_id):
-        dct = {}
-        args = parser.parse_args()
-        data = {'data':args['data']}        
-        dct[todo_id] = data
-        # try:
-        return str(dct)
-        # if request:
-        #     try:
-        #         return str(dir(request))
-        #     except:    
-        #         return request
-        # else:
-        #     return "put request."
-        #     # return request.form
-        # # return "put request."
-        # todos[todo_id] = request.form['data']
-        # return {todo_id: todos[todo_id]}
+        args = parser.parse_args() #args is a dictionary of stuff sent over by the request.
+        return "put request with args = " + str(args) + " and sorted args = " + str(doSomething(args))
 
     def post(self, todo_id):
         args = parser.parse_args()
-        # try:
-        return str(args)
-        # except:
+        return "post request with args = " + str(args) + "and sorted args = " + str(doSomething(args))
 
-        #     if request:
-        #         try:
-        #             return str(dir(request)) + "\n" + str(request.__dict__) + "\n" + str(request.form) + str(request.query_string)
-        #         except:    
-        #             return request
-        #     else:
-        #         return "post request."
 
 api.add_resource(TodoSimple, '/<string:todo_id>', '/')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000)) #not sure this will work
-    app.run(host='0.0.0.0', debug=True, port=port)
+    if os.environ.get('DATABASE_URL'):
+        import database_connection
+        urllib.parse.uses_netloc.append("postgres")
+        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+        app.run(host='0.0.0.0', debug=True, port=port)
+    else:
+        app.run(host='127.0.0.1', debug=True, port=port)
--- testApi-1.py
+++ testApi-1.py
@@ -1,4 +1,32 @@
 from requests import put, get, post
+import os
+
+if os.environ.get('DATABASE_URL'):
+    c = get('http://olin-api.heroku.com/test', params={ 'data': 'data to be sent here', 'a':'b' })
+    print c
+    print c.__repr__
+    print c.json()
+
+else:
+    get_req = get('http://localhost:5000/test', data={ 'data': 'get request data?', 'a':'b', 'c':'d' })
+    print get_req
+    # print get_req.__repr__
+    print get_req.json()
+
+
+    post_req = post('http://localhost:5000/test', data={ 'data': 'post request data', 'a':'b', 'c':'d' })
+    print post_req
+    # print post_req.__repr__
+    print post_req.json()
+
+    put_req = put('http://localhost:5000/test', data={ 'data': 'put request data', 'a':'b', 'c':'d' })
+    print put_req
+    # print put_req.__repr__
+    print put_req.json()
+
+
+### Unused Queries ###
+
 
 # print put('http://localhost:5000/todo1', data={'data': 'Remember the milk'}).json()
 
@@ -13,12 +41,6 @@ from requests import put, get, post
 # #so is b
 # print b.__repr__
 # print b.json()
-
-c = put('http://olin-api.heroku.com/test', params={ 'data': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'a':'b' })
-
-print c.__repr__
-print c.json()
-
 # get('http://localhost:5000/todo2').json()
 # print get('http://olin-api.heroku.com/todo1').json()
 # print get('http://olin-api.heroku.com/todo1', data = {'data': 'select * from events where event_id is not null'}).json()
