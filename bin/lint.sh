fd0982547d3e45f927c4c7ccec4cfa6d78b2e78d
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-03-21 18:16:03 -0400

    improve lint script

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
+# INSTRUCTIONS: ./lint.sh <path to code you want to lint>
+# (otherwise it'll assume you're running from project root)
 source venv/bin/activate &&
- pylint src/app.py
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, using default" &&
+    python3 -m pylint -d invalid-name,no-member,no-self-use,too-few-public-methods src/
+  else
+    python3 -m pylint -d invalid-name,no-member,no-self-use,too-few-public-methods $1
+fi
+
+# -d ignores the errors listed:
+# no-member because flask/mongoengine stuff frequently shows up as false positive
+# no-self-use for flask, which causes many false positives due to things needing to be members
+# too-few-public-methods for flask as well, since some things have to be classes
