de81b970983bbca49d986e701775769d4143691e
Leon Jun Wei Lam <llam@olin.edu>
2017-04-06 21:15:26 -0400

    more error handling, email fields are unique now, centralized test file in /src

--- src/document_models.py
+++ src/document_models.py
@@ -1,5 +1,5 @@
 """ Contains all of the mongoengine models for data held in mongo """
-from mongoengine import Document, StringField, IntField, ListField, DictField
+from mongoengine import Document, StringField, IntField, ListField, DictField, EmailField
 
 class Person(Document):
     """
@@ -27,7 +27,7 @@ class Person(Document):
     fName = StringField(max_length=240, required=True)
     lName = StringField(max_length=240, required=True)
     comYear = IntField()
-    email = StringField(max_length=100, required=True)
+    email = EmailField(max_length=100, required=True, unique=True, sparse=True)
     pronouns = StringField(max_length=100)
     preferredName = StringField(max_length=240)
     services = DictField()
@@ -38,19 +38,20 @@ class Person(Document):
     # allergies/diet
     # image/gravatar
 
-if __name__ == "__main__":
-    from database_connection_mongoengine import make_connection
-    make_connection()
-    Person.drop_collection()
-    people = [
-    {"fName": "John", "lName": "Doe", "comYear": 2015, "email": "JohnDoe@students.olin.edu", "pronouns": "He/Him/His", "preferredName": "John", "services":{"venmo":"jDoe109", "messenger":"Doeboy"}},
-    {"fName": "Jane", "lName": "Doe", "comYear": 2015, "email": "JaneDoe@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Jane"},
-    {"fName": "Alan", "lName": "Smith", "comYear": 2016, "email": "AlanSmith@students.olin.edu", "pronouns": "They/Them/Theirs", "preferredName":"Alan"},
-    {"fName": "Bob", "lName": "Burger", "comYear": 2016, "email": "BobBurger@students.olin.edu", "pronouns": "It/It/Its", "preferredName": "Burger"},
-    {"fName": "Susan", "lName": "Green", "comYear": 2017, "email": "SusanGreen@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Suzie"},
-    {"fName": "Marianne", "lName": "Moss", "comYear": 2017, "email": "MarianneMoss@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Carrie"},
-    ]
+# if __name__ == "__main__":
+#     from database_connection_mongoengine import make_connection
+#     from document_models import Person
+#     make_connection()
+#     Person.drop_collection()
+#     people = [
+#     {"fName": "John", "lName": "Doe", "comYear": 2015, "email": "JohnDoe@students.olin.edu", "pronouns": "He/Him/His", "preferredName": "John", "services":{"venmo":"jDoe109", "messenger":"Doeboy"}},
+#     {"fName": "Jane", "lName": "Doe", "comYear": 2015, "email": "JaneDoe@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Jane"},
+#     {"fName": "Alan", "lName": "Smith", "comYear": 2016, "email": "AlanSmith@students.olin.edu", "pronouns": "They/Them/Theirs", "preferredName":"Alan"},
+#     {"fName": "Bob", "lName": "Burger", "comYear": 2016, "email": "BobBurger@students.olin.edu", "pronouns": "It/It/Its", "preferredName": "Burger"},
+#     {"fName": "Susan", "lName": "Green", "comYear": 2017, "email": "SusanGreen@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Suzie"},
+#     {"fName": "Marianne", "lName": "Moss", "comYear": 2017, "email": "MarianneMoss@students.olin.edu", "pronouns": "She/Her/Hers", "preferredName": "Carrie"},
+#     ]
 
-    for dct in people:
-        item = Person(**dct)
-        item.save()
+#     for dct in people:
+#         item = Person(**dct)
+#         item.save()
--- src/resources/person.py
+++ src/resources/person.py
@@ -4,6 +4,7 @@ database of Olin people """
 from flask import Blueprint, request
 from flask_restful import Resource, Api
 from src.document_models import Person
+from mongoengine.errors import NotUniqueError
 
 # Blueprint setup
 people = Blueprint('people', __name__)
@@ -27,45 +28,66 @@ class PersonEndpoint(Resource):
             comYearMIN  Minimum community year of the user (inclusive)
             comYearMAX  Maximum community year of the user (inclusive)
         """
-        params = request.args
-        query = Person.objects()
-        if params.get('fName'):
-            query = query.filter(fName = params['fName'])
-        if params.get('lName'):
-            query = query.filter(lName = params['lName'])
-        if params.get('email'):
-            query = query.filter(email = params['email'])
-        if params.get('comYearMIN'):
-            query = query.filter(comYear__gte = int(params['comYearMIN']))
-        if params.get('comYearMAX'):
-            query = query.filter(comYear__lte = int(params['comYearMAX']))
-        return [person.to_json() for person in query]
+        try:
+            params = request.args
+            query = Person.objects()
+            if params.get('fName'):
+                query = query.filter(fName = params['fName'])
+            if params.get('lName'):
+                query = query.filter(lName = params['lName'])
+            if params.get('email'):
+                query = query.filter(email = params['email'])
+            if params.get('comYearMIN'):
+                query = query.filter(comYear__gte = int(params['comYearMIN']))
+            if params.get('comYearMAX'):
+                query = query.filter(comYear__lte = int(params['comYearMAX']))
+            return {'response': {'ok': 1.0}, 'searched':[person.to_json() for person in query]}
+
+        except Exception as e:
+            return {'response': {'ok': 0.0, 'error': str(e)}, 'searched': None}
 
     def put(self):
+        try:
+            params = request.args
+            putQuery = Person.objects()
+            if params.get('fName'):
+                putQuery = putQuery.filter(fName = params['fName'])
+            if params.get('lName'):
+                putQuery = putQuery.filter(lName = params['lName'])
+            if params.get('email'):
+                putQuery = putQuery.filter(email = params['email'])
+            if params.get('comYearMIN'):
+                putQuery = putQuery.filter(comYear__gte = int(params['comYearMIN']))
+            if params.get('comYearMAX'):
+                putQuery = putQuery.filter(comYear__lte = int(params['comYearMAX']))
+            edited_ids = [person.id for person in putQuery]
+            serverResponse = putQuery.update(full_result = True, **request.json)
+            return {'response': serverResponse, 'edited': [person.to_json() for person in Person.objects(id__in = edited_ids)]}
+
+        except NotUniqueError: #email is the only unique index users can access, since object id cannot be searched or created manually.
+            return {'response': {'ok': 0, 'error': "Email {} already exists".format(request.json['email'])}, 'edited': None}
+
+        except Exception as e:
+            return {'response': {'ok': 0, 'error': str(e)}, 'edited': None}
+
+
+    def post(self):
         """
         If requests.json's fields do not match those defined in the Person model, this fails. 
         See documentation for src.document_models.Person for more details.
         """
-        object = Person(**request.json) 
-        object.save()
-        return object.to_json()
+        try:
+            object = Person(**request.json) 
+            object.save()
+            return {'response': {'ok': 1.0}, 'created': object.to_json()}
 
+        except NotUniqueError:
+            return {'response': {'ok': 0, 'error': "Email {} already exists".format(request.json['email'])}, 'created': None}
+            
+        except Exception as e:
+            return {'response': {'ok': 0, 'error': str(e)}, 'created': None}
+        
 
-    def post(self):
-        params = request.args
-        postQuery = Person.objects()
-        if params.get('fName'):
-            postQuery = postQuery.filter(fName = params['fName'])
-        if params.get('lName'):
-            postQuery = postQuery.filter(lName = params['lName'])
-        if params.get('email'):
-            postQuery = postQuery.filter(email = params['email'])
-        if params.get('comYearMIN'):
-            postQuery = postQuery.filter(comYear__gte = int(params['comYearMIN']))
-        if params.get('comYearMAX'):
-            postQuery = postQuery.filter(comYear__lte = int(params['comYearMAX']))
-        postQuery.update(**request.json)
-        return "Update Request Received"
 
 
 # Resources
